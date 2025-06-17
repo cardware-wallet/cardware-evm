c563d67b1eb89350c10e5f70ae21f66765fa8f1a
Herman Rademeyer <herman@cardwarewallet.com>
2025-06-17 16:37:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() {
 
     //Transaction History - Structured and Pretty formatted
     let tx_history = wal.transactions("https://api.etherscan.io/v2/api", Some(5)).await;
-    println!("📊 Transaction History (first 5 transactions):");
+    println!("Transaction History (first 5 transactions):");
     println!("{}", tx_history);
     //estimate fee example:
     //Erc20 Balance
